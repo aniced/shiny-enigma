4d6d2623dc987c248f8b780667a98b190472b0aa
Frog Chen <satgo1546@yeah.net>
2018-03-24 11:37:53 +0800

    Base load paths on SDL_GetBasePath

--- base.cpp
+++ base.cpp
@@ -4,6 +4,15 @@
 //   Initialization, application main loop, event handling, etc.
 //=============================================================================
 
+const char* $base_path = NULL;
+
+char* rtp(const char* path) {
+	static char s[256];
+	strcpy(s, $base_path);
+	strcat(s, path);
+	return s;
+}
+
 void init() {
 	if (SDL_Init(0) < 0) error("/* 1st */ SDL_Init() < 0");
 	SDL_SetHint(SDL_HINT_VIDEO_ALLOW_SCREENSAVER, "1");
@@ -13,6 +22,7 @@ void init() {
 	}
 	IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF);
 	if (TTF_Init() == -1) error("TTF_Init() == -1");
+	$base_path = SDL_GetBasePath();
 	$window = SDL_CreateWindow(
 		APPLICATION_TITLE,
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -22,29 +32,38 @@ void init() {
 	SDL_SetWindowMinimumSize($window, 320, 240);
 	$renderer = SDL_CreateRenderer($window, -1, 0);
 	SDL_RenderSetIntegerScale($renderer, SDL_TRUE);
+	// load assets
 	for (int i = 0; i < 2; i++) {
 		char filename[16];
 		sprintf(filename, "tex%d.png", i);
-		$texture[i] = IMG_LoadTexture($renderer, filename);
+		$texture[i] = IMG_LoadTexture($renderer, rtp(filename));
 	}
 	for (int i = 0; i < 1; i++) {
 		char filename[16];
 		sprintf(filename, "fon%d.ttf", i);
-		$font[i] = TTF_OpenFontIndex(filename, 12, 0);
+		$font[i] = TTF_OpenFontIndex(rtp(filename), 12, 0);
 		TTF_SetFontHinting($font[i], TTF_HINTING_NONE);
 	}
 	SDL_ClearError();
+	// initialize Lua
 	L = luaL_newstate();
 	lua_atpanic(L, panic);
 	luaL_openlibs(L);
+	// inject os.exit
 	lua_getglobal(L, "os");
 	lua_pushcfunction(L, quit_lua);
 	lua_setfield(L, -2, "exit");
 	lua_pop(L, 1);
+	// open game libraries
 	Graphics::init();
 	Window::init();
 	Util::init();
-	if (luaL_dofile(L, "main.lua") != 0) panic(L);
+	// execute the main script
+	char* script_filename;
+	script_filename = rtp("main.lua");
+	script_filename = SDL_iconv_utf8_locale(script_filename);
+	if (luaL_dofile(L, script_filename) != 0) panic(L);
+	SDL_free(script_filename);
 }
 
 void loop() {
