7233dd480f206c0217f9498ef2f6084f9131bdea
Frog Chen <satgo1546@yeah.net>
2018-03-31 17:44:00 +0800

    Add Widget_Button

--- main.lua
+++ main.lua
@@ -2,11 +2,15 @@ require 'widget'
 require 'widget_textbase'
 require 'widget_label'
 require 'widget_textbox'
+require 'widget_button'
 
 fullscreen = false
-w = Widget_Textbox.new()
-w:set_text("type here")
-w:set_color({r = 128, g = 0, b = 128})
+w = Widget_Button.new()
+w.rect.x = 100
+w.rect.y = 100
+w.rect.w = 80
+w:set_text("CommandButton1")
+w:set_color({r = 0, g = 128, b = 128})
 
 x = 0
 y = 100
@@ -21,12 +25,10 @@ function on_paint()
 	if x >= 60 then x = 0 end
 	Graphics.copy(Graphics.get_rect(), 0, {x = 255, y = 255, w = 1, h = 1})
 	Graphics.copy({x = x, y = y, w = 10, h = 10}, 0, {x = 0, y = 0, w = 1, h = 1})
-	Graphics.draw_9patch({x = 50, y = 50, w = 80, h = 25}, {texture = 0, x = 0, y = 0, w = 4, h = 4, t = 1, r = 2, b = 2, l = 1})
 	w:draw()
 	local m = Input.mouse_get_point()
 	draw_debug_line(1, string.format("set FPS = %d", Graphics.get_fps()))
 	draw_debug_line(2, string.format("M(%d, %d)", m.x, m.y))
-	draw_debug_line(3, Rect.point_in_rect(Input.mouse_get_point(), {x = 50, y = 50, w = 80, h = 25}) and "Good" or "Not")
 end
 
 Input.text_start()
---
+++ widget_button.lua
@@ -0,0 +1,24 @@
+Widget_Button = Util.class(Widget_TextBase)
+
+function Widget_Button:init()
+	Widget_TextBase.init(self)
+	self:set_text("")
+	self.rect.h = Widget.WLH
+end
+
+function Widget_Button:update()
+end
+
+function Widget_Button:draw()
+	local patch = {texture = 0, x = 0, y = 0, w = 4, h = 4, t = 1, r = 2, b = 2, l = 1}
+	if Rect.point_in_rect(Input.mouse_get_point(), self.rect) then
+		patch.x = patch.x + 5
+		patch.y = patch.y + 1
+	end
+	Graphics.draw_9patch(self.rect, patch)
+	Graphics.copy(
+		{x = self.rect.x, y = self.rect.y},
+		self.texture,
+		self.texture:get_rect()
+	)
+end
