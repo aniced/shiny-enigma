0f2b48b1ca0356c4481b717187fb3f1518a07262
Frog Chen <satgo1546@yeah.net>
2018-04-04 21:12:57 +0800

    Make some tweaks

--- conmode.lua
+++ conmode.lua
@@ -1,5 +1,14 @@
+function draw_line(i, s)
+	local h = Graphics.get_rect().h
+	t = Graphics.render_text(0, s)
+	t:set_color({r = 0, g = 0, b = 0})
+	Graphics.copy({x = 0, y = h - (8 - i) * WLH}, t, t:get_rect())
+end
+
 function on.update()
 end
 
 function on.paint()
+	Graphics.set_color({r = 0, g = 128, b = 0, a = 255})
+	Graphics.fill_rect({x = 0, y = 0, w = 640, h = 480})
 end
--- entry.lua
+++ entry.lua
@@ -4,7 +4,7 @@ require 'binaryfilewriter'
 function on.update()
 	-- only for first frame as this function itself
 	-- will be replaced the moment it is called
-	if #arg > 3 then
+	if #arg >= 1 then
 		dofile 'conmode.lua'
 	else
 		dofile 'main.lua'
