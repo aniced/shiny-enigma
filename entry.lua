76ca41314db5ebf4611c37e1cf1fbb305d2b6702
Frog Chen <satgo1546@yeah.net>
2018-04-05 11:25:53 +0800

    Separate conmode from the root directory

--- entry.lua
+++ entry.lua
@@ -5,7 +5,7 @@ function on.update()
 	-- only for first frame as this function itself
 	-- will be replaced the moment it is called
 	if #arg >= 1 then
-		dofile 'conmode.lua'
+		dofile 'conmode/main.lua'
 	else
 		dofile 'main.lua'
 	end
