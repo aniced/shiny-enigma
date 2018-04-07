f9239f6c23f0fa8279642b85fcfe8a71f23fefad
Frog Chen <satgo1546@yeah.net>
2018-04-07 21:17:58 +0800

    Add ‘Esc = Exit’

--- conmode/scene_hello.lua
+++ conmode/scene_hello.lua
@@ -6,10 +6,13 @@ function Scene_Hello:init()
 	for i = 1, 30 do
 		self.items[i] = Line.new(tostring(math.sqrt(i)))
 	end
-	self:provide_help("Space = Fire a message")
+	self:provide_help("Space = Fire a message; Esc = Exit")
 	function self.on:update()
 		if Input.triggered(44) then
 			self.items[1] = Line.new(SceneManager.call(Scene_Message.new()))
+		end
+		if Input.triggered(41) then
+			os.exit()
 		end
 	end
 end
