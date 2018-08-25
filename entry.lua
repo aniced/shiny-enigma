a7563e7f69bc22d389c89f3c00c24e460ca5f866
Frog Chen <satgo1546@yeah.net>
2018-08-25 15:27:59 +0800

    Test the dialog

--- entry.lua
+++ entry.lua
@@ -2,6 +2,7 @@ dofile 'util.lua'
 Util.dofile 'application'
 Util.dofile 'typesetting'
 
+Dialog.show_choice("啥玩意",{"去","我去"})
 function on.update()
 	-- only for first frame as this function itself
 	-- will be replaced the moment it is called
